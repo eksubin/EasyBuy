9dc59fa8f45324156b6aa9ea22bfd9a2a55f6cd2
eksubin <subinedu@gmail.com>
2015-12-23 13:46:39 +0530

    testing the commit

--- app/src/main/java/com/e_com/subin/e_commerce/About.java
+++ app/src/main/java/com/e_com/subin/e_commerce/About.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class About extends ActionBarActivity {
 
+    //im ot sure about this
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
